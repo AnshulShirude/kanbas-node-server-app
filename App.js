bbce4420b1d7b06f944fa5986cd9080e49da7ea9
Anshul Shirude <34725764+AnshulShirude@users.noreply.github.com>
2024-04-12 00:46:10 -0400

    continued work on backend

--- App.js
+++ App.js
@@ -4,6 +4,9 @@ import Lab5 from './Lab5.js';
 import cors from "cors";
 import CourseRoutes from './Kanbas/courses/routes.js';
 import ModuleRoutes from "./Kanbas/modules/routes.js";
+import mongoose from 'mongoose';
+import UserRoutes from './Users/routes.js';
+mongoose.connect("mongodb://127.0.0.1:27017/kanbas");
 const app = express();
 app.use(cors());
 // app.use(
@@ -14,6 +17,7 @@ app.use(cors());
 // );
 CourseRoutes(app);
 app.use(express.json());
+UserRoutes(app);
 ModuleRoutes(app);
 Lab5(app);
 Hello(app);
