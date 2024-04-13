cc617d7208683493179b1c49e1fc3f05b2e62eed
Anshul Shirude <34725764+AnshulShirude@users.noreply.github.com>
2024-04-13 10:10:31 -0400

    a6 node env

--- App.js
+++ App.js
@@ -14,7 +14,7 @@ mongoose.connect(CONNECTION_STRING);
 const app = express();
 app.use(cors({
     credentials: true,
-    origin: process.env.FRONTEND_URL,
+    origin: process.env.FRONTEND_URL_A6,
 }));
 const sessionOptions = {
     secret: "secret",
